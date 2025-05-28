c602790dbff10ff9a3d9d0259860face0d61c154
Danny Behar <dannybehar@icloud.com>
2025-05-28 14:45:55 -0400

    Initial Commit

---
+++ RecipeHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeHelper
+//
+//  Created by Daniel Behar on 5/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeHelper/RecipeHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeHelperApp.swift
+//  RecipeHelper
+//
+//  Created by Daniel Behar on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
